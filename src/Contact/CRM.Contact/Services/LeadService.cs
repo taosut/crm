f49b3e2eb225cf3df6cead953e6f12e7652cab39
Phuong Le <tungphuongld@gmail.com>
2019-10-21 18:09:34 +0700

    try to debug

--- src/Contact/CRM.Contact/Services/LeadService.cs
+++ src/Contact/CRM.Contact/Services/LeadService.cs
@@ -4,24 +4,30 @@ using CRM.Protobuf.Contacts.V1;
 using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
 using MediatR;
-
+using Microsoft.Extensions.Logging;
 
 namespace CRM.Contact.Services
 {
     public class LeadService : LeadApi.LeadApiBase
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<LeadService> _logger;
 
         public LeadService(Shared.EventBus.Nats.INatsConnection connection,
-            IMediator mediator)
+            IMediator mediator,
+            ILogger<LeadService> logger)
         {
             _mediator = mediator;
 
-            connection.TryConnect();
+            _logger = logger;
+
+            //connection.TryConnect();
         }
 
         public override async Task<PongReply> Ping(Empty request, ServerCallContext context)
         {
+            _logger.LogInformation("Ping handler happens ...");
+
             var result = await _mediator.Send(new PingQuery());
             return result;
         }
